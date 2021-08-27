24768da38de8ccfcac2b4e62ae911ef437e9af6d
KangHayeonn <rkdgkdus522@naver.com>
2021-08-27 17:28:48 +0900

    백준 1260문제 : DFS 성곤 / BFS 미완성

--- Q_BAEKJOON_1260/Q_BAEKJOON_1260/1260.c
+++ Q_BAEKJOON_1260/Q_BAEKJOON_1260/1260.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void DFS(int** arr, int N, int START) {
-	int* dfs_arr = (int*)calloc(N ,sizeof(int));
+void DFS(int** graph, int N, int START, int** dfs_arr) {
 	printf("%d ", START);
-	dfs_arr[START] = 1;
-	for (int i = 0; i < N; i++) {
-		if (arr[i][0] == START && dfs_arr[i] == 0)
-			DFS(arr, N, i);
+	dfs_arr[0][START] = 1;
+	
+	for (int i = 1; i < N + 1; i++) {
+		if (graph[START][i] == 1 && dfs_arr[0][i] == 0) {
+			DFS(graph, N, i, dfs_arr);  // dfs_arr 주솟값 자체를 보냄
+		}
 	}
-
 	return;
 }
 
-void BFS(int*** arr, int N, int START) {
+void BFS(int*** arr, int N, int START, int** bfs_arr) {
 
 }
 
 int main() {
 	int N, M, V;
-	int** arr;
+	int** graph;
+	int j, k;
 
 	scanf("%d %d %d", &N, &M, &V);
 
-	arr = (int**)malloc(sizeof(int*) * M);
+	int* dfs_arr = (int*)calloc(N + 1, sizeof(int));
+	int* bfs_arr = (int*)calloc(N + 1, sizeof(int));
 
-	for (int i = 0; i < M; i++) arr[i] = (int*)malloc(sizeof(int) * 2);
+	graph = (int**)malloc(sizeof(int*)*(N+1));
 
-	for (int i = 0; i < M; i++) 
-		scanf("%d %d", &arr[i][0], &arr[i][1]);
+	for (int i = 0; i < N+1; i++) graph[i] = (int*)calloc(N+1, sizeof(int));
 
-	printf("%d", arr[0][0]);
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d", &j, &k);
+		graph[j][k] = 1;
+		graph[k][j] = 1;
+	}
+	/*
+	for (int i = 0; i < N+1; i++)
+		for (int j = 0; j < N+1; j++)
+			printf("graph[%d][%d] = %d\n", i, j, graph[i][j]);
+	*/
+	DFS(graph, N, V , &dfs_arr); // 이차원 배열처럼 dfs_arr 주솟값 자체를 보냄
+	BFS(&graph, N, V, &bfs_arr);
 
-	for (int i = 0; i < M; i++)
-		for (int j = 0; j < 2; j++)
-			printf("arr[%d][%d] = %d\n", i, j, arr[i][j]);
+	for (int i = 0; i < M; i++) free(graph[i]);
 
-	DFS(arr, N, V);
-	BFS(&arr, N, V);
-
-	for (int i = 0; i < M; i++) free(arr[i]);
-
-	free(arr);
+	free(graph);
+	free(dfs_arr);
+	free(bfs_arr);
 
 	return 0;
 }
